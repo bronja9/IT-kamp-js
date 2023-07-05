a25253ce397ed3820e684ad3bc7ddd3975632251
bronja9 <otraditi@gmail.com>
2023-07-05 16:31:04 +0200

    m

---
+++ Asinhrona/vezba/app.js
@@ -0,0 +1,17 @@
+// ukus neki was selected 2
+// start the production 0
+// cut the fruits 2
+// liquids prvi i drugi was added 1
+// start the machine 1
+// ice cream placed on holder neki 2
+// toppings was selected 3
+// serve ice cream 2
+
+let stocks = {
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "penauts"],
+};
+
+isShopOpen = true;
