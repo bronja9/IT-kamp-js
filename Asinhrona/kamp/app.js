44fd99b2b3d08c8d6de798c2658deea1c85421ad
bronja9 <otraditi@gmail.com>
2023-07-05 11:16:42 +0200

    m

--- Asinhrona/kamp/app.js
+++ Asinhrona/kamp/app.js
@@ -1,18 +1,26 @@
-const firstPromise = new Promise((res, rej) => {
-  let age = prompt("put in your age");
-  setTimeout(() => {
-    if (+age > 17) {
-      res = +age;
-    } else {
-      rej = +age;
-    }
-  }, 400);
-});
+// const firstPromise = new Promise((res, rej) => {
+//   let age = prompt("put in your age");
+//   setTimeout(() => {
+//     if (+age > 17) {
+//       res = +age;
+//     } else {
+//       rej = +age;
+//     }
+//   }, 400);
+// });
 
-firstPromise
-  .then((data) => {
-    console.log(`pesron has been adult for ${data - 18} `);
-  })
-  .catch((data) => {
-    console.log(`pesron has been adult for ${18 - data} `);
-  });
+// firstPromise
+//   .then((data) => {
+//     console.log(`pesron has been adult for ${data - 18} `);
+//   })
+//   .catch((data) => {
+//     console.log(`pesron has been adult for ${18 - data} `);
+//   });
+
+const fetchData = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+
+fetchData();
