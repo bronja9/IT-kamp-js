0abb43ca1b8dff4d82975998bc9f9e86fd19ad87
bronja9 <otraditi@gmail.com>
2023-07-05 18:02:44 +0200

    m

--- Asinhrona/vezba/app.js
+++ Asinhrona/vezba/app.js
@@ -16,6 +16,49 @@ let stocks = {
 
 let isShopOpen = true;
 
+function time(ms) {
+  return new Promise((resolve, reject) => {
+    if (isShopOpen) {
+      setTimeout(resolve, ms);
+    } else {
+      reject(console.log("shop is closed"));
+    }
+  });
+}
+
+async function kitchen() {
+  try {
+    await time(2000);
+    console.log(`${stocks.Fruits[0]} has been selected`);
+
+    await time(0);
+    console.log("start the production");
+
+    await time(2000);
+    console.log("cut the fruits");
+
+    await time(1000);
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+
+    await time(1000);
+    console.log("start the machine");
+
+    await time(2000);
+    console.log(`ice cream has been placed on ${stocks.holder[0]}`);
+
+    await time(3000);
+    console.log(`${stocks.toppings[0]} was selected`);
+
+    await time(2000);
+    console.log("serve the ice cream");
+  } catch (error) {
+    console.log("customer left");
+  } finally {
+    console.log("day ended, shop is closed");
+  }
+}
+
+kitchen();
 /////////////////////////////////////////////////////////////////////////////////////////
 // let order = (work, time) => {
 //   return new Promise((resolve, reject) => {
