96f3c684d97cc679568fa9b4de2cf8094b8d2f15
bronja9 <otraditi@gmail.com>
2023-07-07 11:19:08 +0200

    fs

--- Asinhrona/kamp/app.js
+++ Asinhrona/kamp/app.js
@@ -17,10 +17,42 @@
 //     console.log(`pesron has been adult for ${18 - data} `);
 //   });
 
-const fetchData = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+//////////////////////////////////// FETCH ////////////////////////////////////////////
+
+// const fetchData = () => {
+//   fetch("https://jsonplaceholder.typicode.com/todos")
+//     .then((response) => response.json())
+//     .then((json) => {
+//       console.log(json);
+//       return json.filter((el) => el.id % 2 === 0);
+//     })
+//     .then((salko) => {
+//       console.log(salko);
+//     });
+// };
+
+// fetchData();
+
+// const fetchData2 = () => {
+//   fetch("https://dummyjson.com/products")
+//     .then((res) => res.json())
+//     .then((novi) => {
+//       console.log(novi);
+//       return novi.products.filter((el) => el.stock > 64);
+//     })
+//     .then(console.log);
+// };
+
+// fetchData2();
+
+const fetchData3 = () => {
+  fetch("https://dummyjson.com/carts")
+    .then((res) => res.json())
+    .then((novi) =>
+      novi.carts.map((el) => el.products.filter((el) => el.total > 646))
+    )
+    .then((noviji) => noviji.filter((el) => el.length))
+    .then(console.log);
 };
 
-fetchData();
+fetchData3();
