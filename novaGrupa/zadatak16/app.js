db530b3b528d0a0eddf3db5bf45d489b51148061
bronja9 <otraditi@gmail.com>
2023-07-30 20:25:34 +0200

    sa

--- novaGrupa/zadatak16/app.js
+++ novaGrupa/zadatak16/app.js
@@ -1,1 +1,14 @@
-console.log("dlksa;");
+function rollDice() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+}
+
+function roll(times) {
+  for (let i = 0; i < times; i++) {
+    rollDice();
+  }
+
+  console.log("pozvao sam roll funkciju");
+}
+
+roll(10);
