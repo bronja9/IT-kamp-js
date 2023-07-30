cbd126d01885ac3ee9c858e75d7ea1aec62d4d47
bronja9 <otraditi@gmail.com>
2023-07-30 20:37:15 +0200

    sa

--- novaGrupa/zadatak16/app.js
+++ novaGrupa/zadatak16/app.js
@@ -7,8 +7,24 @@ function roll(times) {
   for (let i = 0; i < times; i++) {
     rollDice();
   }
-
   console.log("pozvao sam roll funkciju");
 }
 
 roll(10);
+
+let niz = [];
+function rollThousand() {
+  var roll = Math.floor(Math.random() * 1000);
+  return roll;
+}
+
+console.log(roll);
+
+function pozivanje(array, number) {
+  for (let i = 0; i < number; i++) {
+    array.push(rollThousand());
+  }
+}
+
+pozivanje(niz, 10);
+console.log(niz);
