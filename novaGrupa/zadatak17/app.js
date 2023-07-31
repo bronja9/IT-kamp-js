de48f57557fd83f753ab4bdd753903f0631b5721
bronja9 <otraditi@gmail.com>
2023-07-31 19:17:58 +0200

    as

--- novaGrupa/zadatak17/app.js
+++ novaGrupa/zadatak17/app.js
@@ -1,1 +1,14 @@
-console.log("daksjls");
+function isValidPassword(password, username) {
+  let password = password.toUpperCase();
+  let username = username.toUpperCase();
+
+  if (
+    password.length > 7 &&
+    !password.include(username) &&
+    !password.include(" ")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
