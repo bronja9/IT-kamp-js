4eeb3153d24359fd3b40b9ecf71106cdb5726238
bronja9 <otraditi@gmail.com>
2023-08-02 20:39:29 +0200

    sa

--- novaGrupa/zadatak18/app.js
+++ novaGrupa/zadatak18/app.js
@@ -87,5 +87,18 @@ resultFunction();
 
 ////////////// pangam
 
-console.log(isPangam("sadkjsalkdjs"));
-console.log("dksajl");
+// pangram
+function isPangram(string) {
+  let strArr = string.toLowerCase();
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (strArr.indexOf(alphabet[i]) < 0) {
+      return false;
+    }
+  }
+  console.log("its pangram");
+  return true;
+}
+
+isPangram("The quick brown fox jumps over the lazy dog");
