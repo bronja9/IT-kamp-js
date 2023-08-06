929e8183fc2d7fc95624738a938e3e7eb18be795
bronja9 <otraditi@gmail.com>
2023-08-06 18:52:47 +0200

    sa

--- novaGrupa/zadatak20/app.js
+++ novaGrupa/zadatak20/app.js
@@ -1,1 +1,13 @@
-console.log("lkdas;");
+const fruitArray = ["apple", "banana", "watermelon", "kiwi"];
+let nizNovi = [];
+
+fruitArray.forEach((e, i, array) => {
+  console.log(
+    array,
+    "ovo je niz prosledjen u call back funkciju for each metode"
+  );
+  nizNovi.push(e.toUpperCase());
+  return;
+});
+
+console.log(nizNovi, "saaaaaaaaaaaaaaaaaaaad");
