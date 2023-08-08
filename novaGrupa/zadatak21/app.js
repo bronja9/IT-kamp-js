7e82ae94edbe3e7ab106cb536d56ec7e05bb2229
bronja9 <otraditi@gmail.com>
2023-08-08 21:30:07 +0200

    sa

--- novaGrupa/zadatak21/app.js
+++ novaGrupa/zadatak21/app.js
@@ -1,1 +1,13 @@
-console.log("dksaj");
+const arrayOfNumbers = [
+  1, 2, 3, -2, -4, -10, 1, 5, 67, 1321, -2, -223, -4, -6, 23,
+];
+
+//Zadatak 4: Pronalaženje prvog pozitivnog broja
+//Napišite funkciju koja pronalazi i vraća prvi pozitivan broj u nizu. Ako nema pozitivnih brojeva, funkcija treba vratiti null.
+
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+  if (arrayOfNumbers[i] > 0) {
+    console.log(arrayOfNumbers[i]);
+  }
+  break;
+}
