08c1eb34b236ba8fd92b50e55c25c02aad641c62
bronja9 <otraditi@gmail.com>
2023-08-11 20:03:48 +0200

    sa

--- novaGrupa/zadatak22/app.js
+++ novaGrupa/zadatak22/app.js
@@ -1,1 +1,20 @@
-console.log("dksal");
+let niz = [12, -12, 5, -5, 6, -6, 7, -7, 8, -20, 20, 50, -30];
+
+// napisati funkciju koja ce da proveri da li za svaki pozitivan broj ima isti takav
+//negativan broj, i ako ima, smestiti te pozitivne brojeve
+//u novi niz i taj niz ispisati.
+
+function checkAntonims(array) {
+  let antonimPositive = [];
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (array[i] - array[i] * 2 === array[j]) {
+        antonimPositive.push(array[i]);
+      }
+    }
+  }
+  return antonimPositive;
+}
+
+console.log(checkAntonims(niz));
