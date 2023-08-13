0cbd76ab4b909e920e70240e00e4bac3a1815c61
bronja9 <otraditi@gmail.com>
2023-08-13 19:14:49 +0200

    sa

--- novaGrupa/zadatak24/app.js
+++ novaGrupa/zadatak24/app.js
@@ -1,14 +1,23 @@
 //1
 const DATA1 = [2, 26, 38, 75, 11, 29];
-const DATA2 = [, "a", "b", "c", "d", "e"];
+const DATA2 = ["a", "b", "c", "d", "e"];
 
-const helper = DATA1 + DATA2;
-console.log(helper);
+function twoArrays(first, second) {
+  let helper = first.concat(second);
 
-const evenNums = [];
-for (let i = 0; i < DATA1.length; i++) {
-  if (DATA1[i] % 2 === 0) {
-    evenNums.push(DATA1[i]);
+  let evenNums = [];
+
+  for (let i = 0; i < helper.length; i++) {
+    if (typeof helper[i] === "number" && helper[i] % 2 === 0) {
+      evenNums.push(helper[i]);
+    }
   }
+
+  evenNums.unshift(10, 20);
+
+  return [helper, evenNums];
 }
-console.log(evenNums);
+
+console.log(twoArrays(DATA1, DATA2));
+
+//2
