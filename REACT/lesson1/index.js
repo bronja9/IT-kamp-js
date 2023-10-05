16f176a2bbec4f27d337b57534dc28ceeacb9f1e
bronja9 <otraditi@gmail.com>
2023-10-05 17:21:43 +0200

    sa

--- REACT/lesson1/index.js
+++
@@ -1,9 +0,0 @@
-ReactDOM.render(
-  <ul>
-    <li>hello</li>
-    <li>bonjorno</li>
-    <li>chao</li>
-  </ul>,
-  document.getElementById("root")
-);
-ReactDOM.render(<h1>This is JSX</h1>, document.getElementById("root"));
